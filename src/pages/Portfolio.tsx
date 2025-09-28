f0dc453ea1cd8e8e92d11f605fb2b774493a0195
michaelombiri <michaelombiri2024@gmail.com>
2025-09-28 17:02:18 +0300

    Updated Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -12,7 +12,7 @@ const Portfolio = () => {
       category: 'residential',
       location: 'Eldoret, Uasin Gishu County',
       year: '2022',
-      image: './rsidential.JPG', //'https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=800',
+      image: './rsidential.png', //'https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=800',
       description: 'Complete modern mansion with swimming pool and landscaping.'
     },
     {
