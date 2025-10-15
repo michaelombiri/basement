406ac030430d44cb54d5fa1897df7f9efabd7601
michaelombiri <michaelombiri2024@gmail.com>
2025-10-15 22:44:48 +0300

    Updated Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -165,7 +165,7 @@ const Services = () => {
       </section>
 
       {/* Coverage Area */}
-      <section className="py-20 bg-white">
+      <section className="py-5 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
             <div>
