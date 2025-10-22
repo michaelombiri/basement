6c66e79c07ab95787851b08a09be83255b48ed91
michaelombiri <michaelombiri2024@gmail.com>
2025-10-22 15:08:28 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -109,7 +109,7 @@ const Footer = () => {
               <div className="flex items-center">
                 <Mail className="h-5 w-5 text-amber-600 mr-3 flex-shrink-0" />
                 <a 
-                  href="info@basementcontractors.co.ke" 
+                  href="mailto:info@basementcontractors.co.ke" 
                   className="text-gray-400 hover:text-amber-600 transition-colors duration-300"
                 >
 info@basementcontractors.co.ke
